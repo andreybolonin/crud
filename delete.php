e0eb166862e99ab4aa39f8d52a0945593001acbe
a.bolonin <andreybolonin1989@gmail.com>
2026-01-30 08:26:00 +0300

    upd devcontainer.json

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
 if ($_GET['id']) {
-    $pdo = new PDO("pgsql:host=database;dbname=article", 'user', 'password');
+    $pdo = new PDO("mysql:host=127.0.0.1;dbname=article", 'user', 'password');
     $stmt = $pdo->prepare('DELETE FROM article WHERE id = :id');
     $stmt->bindValue(':id', $_GET['id']);
     $stmt->execute();
